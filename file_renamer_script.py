149625abae6d6c33de59970977d124c221e0cec7
Wes Chow <wgesh8@gmail.com>
2020-01-31 23:53:01 -0800

    Add file

---
+++ file_renamer_script.py
@@ -0,0 +1,16 @@
+import os
+
+# todo??: add folder support, check for other exceptions, code other conversions for different naming conventions
+
+folder_files = os.listdir()  # get list of files in working dir
+
+for file in folder_files:  # go through each file
+    if os.path.isfile(file):  # check if 'file' is a file (not folder)
+        if not file == os.path.basename(__file__):  # check if 'file' is this script
+            file_rename = file.replace("_", " ").title()  # replace underscores with spaces & capitalize first char of words
+            file_rename = file_rename[:file_rename.index(".") + 1] + file_rename[
+                file_rename.index(".") + 1].lower() + file_rename[file_rename.index(".") + 2:]  # set file extension back to lowercase
+
+            os.rename(file, file_rename)  # rename the file
+    else:
+        continue
